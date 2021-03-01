5ec1f53d848666a267e9ed600a38828813a455b5
alext111 <49249379+alext111@users.noreply.github.com>
2021-03-01 16:20:32 -0500

    Added state and country code functionality

--- weatherapp/weather/utils.py
+++ weatherapp/weather/utils.py
@@ -4,11 +4,21 @@ from datetime import date
 #api key and city id for openweathermapapi
 api_key = '9d899b36cc540f63dae387657f3b1c1a'
 
-def getWeather(city_name):
+def getWeather(city_name,state_code,country_code):
     # obtains api data in json format
-    weatherData = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' + api_key)
+    if country_code != "" and state_code != "":
+        weatherData = requests.get(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + ',' + state_code + ',' + country_code + '&appid=' + api_key)
+    elif state_code != "":
+        weatherData = requests.get(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + ',' + state_code + '&appid=' + api_key)
+    elif country_code != "":
+        weatherData = requests.get(
+            'http://api.openweathermap.org/data/2.5/weather?q=' + city_name + ',' + country_code + '&appid=' + api_key)
+    else:
+        weatherData = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city_name + '&appid=' + api_key)
     weatherData = weatherData.json()
-
+    print(weatherData)
     # convert temperatures from kelvin to fahrenheit
     tempMaxFahrenheit = (weatherData['main']['temp_max'] - 273.15) * 1.8 + 32
     tempMinFahrenheit = (weatherData['main']['temp_min'] - 273.15) * 1.8 + 32
--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -14,7 +14,9 @@ def weather_info(request):
 def search(request):
     if request.method == 'POST':
         city_name = request.POST.get("city_name", None)
-        data = getWeather(str(city_name))
+        state_code = request.POST.get("state_code", None)
+        country_code = request.POST.get("country_code", None)
+        data = getWeather(str(city_name),str(state_code),str(country_code))
 
         # return user to required page
         return render(request, 'weather_search_result.html', {'search_data': data})
